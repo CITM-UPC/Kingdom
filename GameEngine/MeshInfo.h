632219eb808879fa858998fa1747f50401a1f304
xGauss05 <jcacaynes@gmail.com>
2023-12-15 23:10:42 +0100

    Added serialize, deserialize function for the MeshInfo.
    - Still not implemented

--- GameEngine/MeshInfo.h
+++ GameEngine/MeshInfo.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <vector>
+#include <ostream>
+#include <istream>
 #include "Engine_Globals.h"
 
+using namespace std;
 class MeshInfo
 {
 public:
@@ -29,4 +32,39 @@ public:
 	std::vector<vec3f> mNormals;
 	std::vector<vec3f> mFaceCenters;
 	std::vector<vec3f> mFaceNormals;
+
+	ostream& serialize(ostream& os) const {
+		//size_t vLength = vertex_data.size();
+		//os.write((char*)&vLength, sizeof(vLength));
+		//os.write((char*)vertex_data.data(), vertex_data.size() * sizeof(VertexV3T2));
+
+		//size_t iLength = index_data.size();
+		//os.write((char*)&iLength, sizeof(iLength));
+		//os.write((char*)index_data.data(), index_data.size() * sizeof(uint));
+
+		return os;
+	}
+
+	istream& deserialize(istream& is) {
+		//size_t temp = 0;
+		//is.read((char*)&temp, sizeof(temp));
+		//vertex_data.resize(temp); // allocates temp amount of cells and changes the length of the vector
+		//is.read((char*)vertex_data.data(), temp * sizeof(VertexV3T2));
+
+		//is.read((char*)&temp, sizeof(temp));
+		//index_data.resize(temp);
+		//is.read((char*)index_data.data(), temp * sizeof(uint));
+
+		return is;
+	}
+
 };
+
+inline ostream& operator<<(ostream& os, const MeshInfo& mesh) {
+	return mesh.serialize(os);
+
+}
+
+inline istream& operator>>(istream& is, MeshInfo& mesh) {
+	return mesh.deserialize(is);
+}
